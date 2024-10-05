9c8e817d227b5bad419734cb9df4a6d52f2701a3
Michael L <mlorenz1012@aol.com>
2024-10-04 23:40:52 -0400

    Set up Spring Boot project with dependencies

---
+++ speechsearch/src/main/java/com/lorenz/speechsearch/SpeechsearchApplication.java
@@ -0,0 +1,13 @@
+package com.lorenz.speechsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // this is a Springboot Application
+public class SpeechsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechsearchApplication.class, args);
+	}
+
+}
---
+++ speechsearch/src/test/java/com/lorenz/speechsearch/SpeechsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenz.speechsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
