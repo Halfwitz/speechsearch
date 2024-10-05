d07dec1fa5843dbbae0c7845ccd31958fea9c77a
Michael L <mlorenz1012@aol.com>
2024-10-05 11:54:44 -0400

    Add findBySpeechId

--- speechsearch/src/main/java/com/lorenz/speechsearch/repository/SnippetRepository.java
+++ speechsearch/src/main/java/com/lorenz/speechsearch/repository/SnippetRepository.java
@@ -4,8 +4,11 @@ import com.lorenz.speechsearch.model.Snippet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SnippetRepository extends JpaRepository<Snippet, Long> {
+    List<Snippet> findBySpeechId(Long speechId);
     // query methods
 
 }
